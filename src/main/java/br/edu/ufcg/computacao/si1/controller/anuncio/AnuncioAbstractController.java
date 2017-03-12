02d62817122fa88a6bb4a33d52225588ac7a4374
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2017-03-12 19:02:55 -0300

    Implementado FR001

--- src/main/java/br/edu/ufcg/computacao/si1/controller/anuncio/AnuncioAbstractController.java
+++ src/main/java/br/edu/ufcg/computacao/si1/controller/anuncio/AnuncioAbstractController.java
@@ -1,5 +1,10 @@
 package br.edu.ufcg.computacao.si1.controller.anuncio;
 
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,15 +12,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.edu.ufcg.computacao.si1.model.Anuncio;
 import br.edu.ufcg.computacao.si1.model.form.AnuncioForm;
 import br.edu.ufcg.computacao.si1.repository.AnuncioRepository;
 import br.edu.ufcg.computacao.si1.service.AnuncioServiceImpl;
 
 @Controller
-public abstract class AnuncioAbstractController  {
+public abstract class AnuncioAbstractController {
 
   @Autowired
   protected AnuncioServiceImpl anuncioService;
@@ -34,8 +41,31 @@ public abstract class AnuncioAbstractController  {
     return model;
   }
 
+  @RequestMapping(value = "/listar/buscar", method = RequestMethod.GET)
+  public ModelAndView getBuscaAnuncios(@RequestParam String chave, @RequestParam Optional<String> nome,
+      @RequestParam Optional<String> categoria) {
+    
+    Set <Anuncio> anuncios = new LinkedHashSet<Anuncio>();
+    
+    if (nome != null) {
+      anuncios.addAll(anuncioService.getAnuncioRepository().findByTitulo(chave));
+    }
+    
+    if (categoria != null) {
+      anuncios.addAll(anuncioService.getAnuncioRepository().findByCategoria(chave));
+    }
+    
+    ModelAndView model = new ModelAndView();
+    model.setViewName("sharedProfile/listar_anuncios");
+    
+    model.addObject("anuncios", anuncios);
+
+    return model;
+  }
+
   public abstract ModelAndView cadastroAnuncio(@Valid AnuncioForm anuncioForm, BindingResult result,
       RedirectAttributes attributes);
+
   public abstract ModelAndView getPageCadastrarAnuncio(AnuncioForm anuncioForm);
 
 }
--- src/main/java/br/edu/ufcg/computacao/si1/repository/AnuncioRepository.java
+++ src/main/java/br/edu/ufcg/computacao/si1/repository/AnuncioRepository.java
@@ -1,11 +1,17 @@
 package br.edu.ufcg.computacao.si1.repository;
 
 import br.edu.ufcg.computacao.si1.model.Anuncio;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by Marcus Oliveira on 28/12/16.
  */
 public interface AnuncioRepository extends JpaRepository<Anuncio, Long> {
+  
+  List<Anuncio> findByTitulo(String titulo);
+  List<Anuncio> findByCategoria(String categoria);
 
 }
