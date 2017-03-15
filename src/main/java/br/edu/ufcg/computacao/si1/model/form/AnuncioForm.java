8e35cc18d3cb75723df86eabeda4daed577775f8
xf <alekssandro.barbosa@ccc.ufcg.edu.br>
2017-03-15 18:30:05 -0300

    remove useless static variable

--- src/main/java/br/edu/ufcg/computacao/si1/model/form/AnuncioForm.java
+++ src/main/java/br/edu/ufcg/computacao/si1/model/form/AnuncioForm.java
@@ -1,6 +1,5 @@
 package br.edu.ufcg.computacao.si1.model.form;
 
-
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.DecimalMin;
@@ -9,59 +8,57 @@ import javax.validation.constraints.Size;
 
 public class AnuncioForm {
 
-    private static final String[] tipos = new String[] {"movel", "imovel", "emprego"};
+	private static final String[] tipos = new String[] { "movel", "imovel", "serviço", "emprego" };
 
-    @NotNull(message = "O titulo não pode ser nulo.")
-    @NotEmpty(message = "O titulo não pode esta vazio.")
-    
-    @Size(min = 2, max = 100, message = "O titulo deve ter entre 2 e 100 caracters")
-    private String titulo;
-    
-    @NotNull(message = "O preçço não pode ser nulo.")
-    @DecimalMin(value = "0.1", message = "O preço minimo é 0.1 para um anúncio.")
-    private Double preco;
-    
-    @NotNull(message = "O tipo de anúncio não pode ser nulo.")
-    @NotEmpty(message = "Escolha um tipo para o anúncio.")
-    private String tipo;
-    
-    @Size(min = 2, max = 100, message = "A categoria deve ter entre 2 e 100 caracters")
-    private String categoria;
-    
-    public String getCategoria() {
-      return categoria;
-    }
+	@NotNull(message = "O titulo não pode ser nulo.")
+	@NotEmpty(message = "O titulo não pode esta vazio.")
+	@Size(min = 2, max = 100, message = "O titulo deve ter entre 2 e 100 caracters")
+	private String titulo;
 
-    public void setCategoria(String categoria) {
-      this.categoria = categoria;
-    }
+	@NotNull(message = "O preçço não pode ser nulo.")
+	@DecimalMin(value = "0.1", message = "O preço minimo é 0.1 para um anúncio.")
+	private Double preco;
 
+	@NotNull(message = "O tipo de anúncio não pode ser nulo.")
+	@NotEmpty(message = "Escolha um tipo para o anúncio.")
+	private String tipo;
 
-    public String getTitulo() {
-        return titulo;
-    }
+	@Size(min = 2, max = 100, message = "A categoria deve ter entre 2 e 100 caracters")
+	private String categoria;
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public String getCategoria() {
+		return categoria;
+	}
 
-    public Double getPreco() {
-        return preco;
-    }
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 
-    public void setPreco(Double preco) {
-        this.preco = preco;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public String getTipo() {
-        return tipo;
-    }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+	public Double getPreco() {
+		return preco;
+	}
 
-    public static String[] getTipos() {
-        return tipos;
-    }
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public static String[] getTipos() {
+		return tipos;
+	}
 }
